1aed7b12a7921304745aebb49eb3b3ee99ad5b47
Sunny Bakalov <sunny.bakalov@gmail.com>
2018-11-11 13:40:16 -0700

    website basically laid out and semi-styled

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+console.log("what up fam")
+
+//declaring variables. these are the numbers that we start the game at.
+let goalNumber = 64;
+let score = 0
+let wins = 0
+let losses = 0
+
+var pictureValue = [10, 4, 8, 7];
+
+//displaying the starting numbers on the HTML page
+$("#goalNumber").text(goalNumber);
+$("#currentScore").text(score);
+$("#myWins").text(wins);
+$("#myLosses").text(losses);
